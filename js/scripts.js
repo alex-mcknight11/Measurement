a9bdad5cd9d927b4162a34f5bdddb0c544b62637
alex-mcknight11 <alex.mcknight11@gmail.com>
2022-01-10 11:54:22 -0800

    add gallon to liter conversion
    Co-Authored-By: Chase Stokes <chasehstokes@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+const g = parseInt(prompt("Enter amount in gallons"));
+  function galToLit(g){
+    return "Measurement in liters is "+(g * 3.7854);
+  }
+  alert(galToLit(g));
+
+
+
+
+
+
+
+
+//1 gallon = 3.7854 liters (gallons * 3.7854)
